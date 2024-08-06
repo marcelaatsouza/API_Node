88d570754bd2a3a12821628f49485e7aef2d60ce
Marcela <marcela.ataidesouza@gmail.com>
2024-08-06 16:50:47 -0300

    Creating models

--- src/app.js
+++ src/app.js
@@ -1,53 +1,49 @@
 import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import livro from "./models/livro.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+  console.error("Erro de conexão.", erro);
+});
+
+conexao.once("open", () => {
+  console.log("Conexão realizada com sucesso!");
+})
 
 const app = express();
 app.use(express.json());
 
-const livros = [
-    {
-        id: 1,
-        titulo: "Frankenstein",
-    },
-    {
-        id: 2,
-        titulo: "Drácula",
-    }
-];
-
-function buscaLivro(id) {
-    return livros.findIndex(livros => {
-        return livros.id === Number(id);
-    });
-}
-
 app.get("/", (req, res) => {
-    res.status(200).send("API em Node.js")
+  res.status(200).send("API Node.js");
 });
 
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
+app.get("/livros", async (req, res) => {
+  const listaLivros = await livro.find({});
+  res.status(200).json(listaLivros);
 });
 
 app.get("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index]);
-});
+  const index = buscaLivro(req.params.id);
+  res.status(200).json(livros[index]);
+})
 
 app.post("/livros", (req, res) => {
-    livros.push(req.body);
-    res.status(201).send("Livro cadastrado com sucesso!")
+  livros.push(req.body);
+  res.status(201).send("Livro cadastrado.");
 });
 
 app.put("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.status(200).json(livros);
+  const index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.status(200).json(livros);
 });
 
 app.delete("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros.splice(index, 1);
-    res.status(200).send("Livro removido.");
+  const index = buscaLivro(req.params.id);
+  livros.splice(index, 1);
+  res.status(200).send("Livro removido.");
 });
 
 export default app;
