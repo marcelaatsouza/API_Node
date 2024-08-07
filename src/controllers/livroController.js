11338fcbc60a1c98a5ea1f055e1d3f12caaa010f
Marcela <marcela.ataidesouza@gmail.com>
2024-08-07 14:52:44 -0300

    Updating routes

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -2,8 +2,32 @@ import livro from "../models/livro.js";
 
 class LivroController {
     static async listarLivros (req, res) {
-        const listaLivros = await livro.find({});
-        res.status(200).json(listaLivros);
+        try {
+            const listaLivros = await livro.find({});
+            res.status(200).json(listaLivros);
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - Falha na requisição.` })
+        }
+    };
+
+    static async listarLivroPorId (req, res) {
+        try {
+            const id = req.params.id;
+            const livroEncontrado = await livro.findById(id);
+            res.status(200).json(livroEncontrado);
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - Falha na requisição do livro.` })
+        }
+    };
+
+    static async atualizarLivro (req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(200).json({ message: "Livro atualizado!" });
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - Falha na atualização do livro.` })
+        }
     };
 
     static async cadastrarLivro (req, res) {
@@ -12,6 +36,16 @@ class LivroController {
             res.status(201).json({ message: "Criado com sucesso!", livro: novoLivro })
         } catch (erro) {
             res.status(500).json({ message: `${erro.message} - Falha ao cadastrar livro.` })
+        }
+    };
+
+    static async excluirLivro (req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndDelete(id);
+            res.status(200).json({ message: "Livro excluído com sucesso."})
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - Falha ao excluir o livro.` })
         }
     };
 };
