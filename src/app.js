81a04e7ba77b4fe8cab5c1853565e7dc37a2448a
Marcela <marcela.ataidesouza@gmail.com>
2024-08-07 13:15:59 -0300

    Creating controllers and routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import conectaNaDatabase from "./config/dbConnect.js";
-import livro from "./models/livro.js";
+import routes from "./routes/index.js";
 
 const conexao = await conectaNaDatabase();
 
@@ -13,16 +13,7 @@ conexao.once("open", () => {
 })
 
 const app = express();
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.status(200).send("API Node.js");
-});
-
-app.get("/livros", async (req, res) => {
-  const listaLivros = await livro.find({});
-  res.status(200).json(listaLivros);
-});
+routes(app);
 
 app.get("/livros/:id", (req, res) => {
   const index = buscaLivro(req.params.id);
